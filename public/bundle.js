129da23c5da665c632e2961dfaa567c1ebd86e1c
Devon McGrath <devon.mcgrath@gmail.com>
2016-06-10 12:14:30 +1200

    added input button

--- public/bundle.js
+++ public/bundle.js
@@ -1674,7 +1674,7 @@
 	},"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
 	    var stack1;
 	
-	  return "<div id=\"list\">\n"
+	  return "<form action=\"/createChallenge\" method=\"post\">\n    <input type=\"input\" name=\"create\" value=\"\" placeholder=\"create new challenge\">\n    <button type=\"button\" name=\"createButton\">Create</button>\n</form>\n\n<div id=\"list\">\n"
 	    + ((stack1 = helpers.each.call(depth0 != null ? depth0 : {},(depth0 != null ? depth0.challenges : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
 	    + "</div>\n";
 	},"useData":true});

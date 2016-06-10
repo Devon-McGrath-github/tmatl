c1a935e0ec23ff9e39f79c95acc8dfec149befdd
Devon McGrath <devon.mcgrath@gmail.com>
2016-06-10 14:01:42 +1200

    implement tables in .hbs

--- public/bundle.js
+++ public/bundle.js
@@ -1668,17 +1668,17 @@
 	module.exports = (Handlebars["default"] || Handlebars).template({"1":function(container,depth0,helpers,partials,data) {
 	    var helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 	
-	  return "        <li>\n            "
+	  return "        <tr>\n            <td>\n                "
 	    + alias4(((helper = (helper = helpers.challenge || (depth0 != null ? depth0.challenge : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"challenge","hash":{},"data":data}) : helper)))
-	    + "\n            <form action='/delete' method=\"post\">\n                <button class=\"delete\" type=\"text\" name=\"id\" value=\""
+	    + "\n            </td>\n            <td>\n                <form action='/delete' method=\"post\">\n                    <button class=\"delete\" type=\"text\" name=\"id\" value=\""
 	    + alias4(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"id","hash":{},"data":data}) : helper)))
-	    + "\">Delete</button>\n            </form>\n        </li>\n";
+	    + "\">Delete</button>\n                </form>\n            </td>\n        </tr>\n";
 	},"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
 	    var stack1;
 	
-	  return "<form action=\"/createChallenge\" name=\"createForm\" method=\"post\">\n    <input type=\"input\" name=\"create\" value=\"\" placeholder=\"create new challenge\">\n    <button type=\"submit\" name=\"createButton\">Create</button>\n</form>\n\n<div id=\"list\">\n"
+	  return "<form action=\"/createChallenge\" name=\"createForm\" method=\"post\">\n    <input type=\"input\" name=\"create\" value=\"\" placeholder=\"create new challenge\">\n    <button type=\"submit\" name=\"createButton\">Create</button>\n</form>\n\n<div id=\"list\">\n    <table>\n"
 	    + ((stack1 = helpers.each.call(depth0 != null ? depth0 : {},(depth0 != null ? depth0.challenges : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
-	    + "</div>\n";
+	    + "    </table>\n";
 	},"useData":true});
 
 /***/ },
